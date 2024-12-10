616fc3dbd9e1af1579ecc58b023844549e23561e
Соня Cавкова <silva.vvss12@gmail.com>
2024-12-10 18:31:39 +0200

    changes in settins.py allowed_hosts and debug

--- app/settings.py
+++ app/settings.py
@@ -26,9 +26,9 @@ load_dotenv(dotenv_path=BASE_DIR / '.env')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
